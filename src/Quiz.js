7ccb6930644ebced8d5fd164ae8df9a2edfdfe83
Marcelo Acevedo <marceloac@microsoft.com>
2022-03-22 17:34:03 -0300

    fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     return (
       <div>
         <div className="QuizQuestion">
-          {quizData.quiz_questions.instruction_test}
+          {quizData.quiz_questions[0].instruction_text}
         </div>
       </div>
     );
